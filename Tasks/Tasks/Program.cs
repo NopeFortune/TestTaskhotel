6573753b6dff4b0582eb8f234bba398cc6abb0fb
NopeFortune <nuclearindustry@gmail.com>
2021-10-12 23:07:47 +0300

    added solution for first task

---
+++ Tasks/Tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] stringy = new string[] { "Moscow", "St.Petersburg", "Voronezsh" };
+            Console.WriteLine(FirstTask(stringy));
+        }
+
+        public static string FirstTask(string[] array)
+        {
+            return $"{string.Join(", ", array)}.";
+        }
+    }
+}
